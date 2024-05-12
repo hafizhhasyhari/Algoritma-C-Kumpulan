d87e4d3b28fc40f9139f06c34aba8b20a23759a7
Hafizh H Asyhari <81144031+hafizhhasyhari@users.noreply.github.com>
2024-05-12 22:29:15 +0700

    Create Belajar 9 Kalkulator Bangun Ruang.cpp

---
+++ Belajar 9 Kalkulator Bangun Ruang.cpp
@@ -0,0 +1,24 @@
+// Kalkulator Bangun Ruang //
+// Dengan semangat tetap belajar //
+// Author : Hafizh H Asyhari //
+#include <iostream>
+using namespace std;
+
+double hitungLuasPersegiPanjang(double panjang, double lebar) {
+    return panjang * lebar;
+}
+
+double hitungVolumeBalok(double panjang, double lebar, double tinggi) {
+    return panjang * lebar * tinggi;
+}
+
+int main() {
+    double panjang, lebar, tinggi;
+    cout << "Mauskkan panjang persegi panjang : ";
+    cin >> panjang;
+    cout << "Mauskkan panjang persegi panjang :";
+    cin >> lebar;
+
+    double luas = hitungLuasPersegiPanjang(panjang, lebar);
+    return 0;
+}
